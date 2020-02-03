0b255baa7263ce83c4035c25d50444b3421c407f
Nikita Shenkman <nikita.shenkman@gmail.com>
2020-02-03 18:39:04 -0500

    bump the versions

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 setup(
     # Because django-statsd was taken, I called this django-statsd-mozilla.
     name='django-statsd-mozilla',
-    version='0.3.15.14',
+    version='0.3.15.15',
     description='Django interface with statsd',
     long_description=open('README.rst').read(),
     author='Andy McKay',
     author_email='andym@mozilla.com',
     license='BSD',
-    install_requires=['statsd>=3.2.2.6'],
+    install_requires=['statsd>=3.2.2.7'],
     packages=['django_statsd',
               'django_statsd/patches',
               'django_statsd/clients',
